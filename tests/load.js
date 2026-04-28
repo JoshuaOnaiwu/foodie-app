13edb5e9122663adfbb5b5294d136c4709d7545b
Joshua Onaiwu <onaiwujoshua@gmail.com>
2026-04-28 15:52:50 +0100

    Add k6 load testing to CI/CD pipeline

---
+++ tests/load.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  const url = __ENV.TARGET_URL;
+
+  const res = http.get(url);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+
+  sleep(1);
+}
