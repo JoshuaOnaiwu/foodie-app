246533bc078dea0deac671bbc7c94589e869b623
Joshua Onaiwu <onaiwujoshua@gmail.com>
2026-04-30 14:41:34 +0100

    trigger k6 autoscaling test

--- tests/load.js
+++ tests/load.js
@@ -1,29 +1,27 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-
-const BASE_URL = __ENV.TARGET_URL || 'http://foodie-alb-36067267.us-east-1.elb.amazonaws.com';
-
 export const options = {
   stages: [
-    { duration: '1m', target: 50 },    // warm up
-    { duration: '2m', target: 150 },   // medium load
-    { duration: '3m', target: 300 },   // heavy load
-    { duration: '3m', target: 500 },   // force scaling
-    { duration: '2m', target: 0 },     // cool down.
+    { duration: '1m', target: 100 },
+    { duration: '2m', target: 300 },
+    { duration: '3m', target: 600 },
+    { duration: '3m', target: 800 }, // push hard
+    { duration: '2m', target: 0 },
   ],
 };
 
 export default function () {
-  if (!BASE_URL) {
-    throw new Error('TARGET_URL is not set');
-  }
+  const url = __ENV.TARGET_URL;
 
-  // 🔥 Multiply load per user (key for frontend apps)
-  http.get(BASE_URL);
-  http.get(BASE_URL);
-  http.get(BASE_URL);
-  http.get(BASE_URL);
-  http.get(BASE_URL);
+  // 🔥 MUCH heavier load
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
+  http.get(url);
 
   sleep(1);
 }
